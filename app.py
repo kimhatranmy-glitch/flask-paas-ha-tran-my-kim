a58717ecda9fef696bcedc55be6608c90471e890
kimhatranmy-glitch <kimhatranmy@gmail.com>
2026-09-04 11:54:48 +0700

    feat: update tieu de v2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     .box {{ background:#DEEAF1; border-left: 5px solid #1F4E79; padding: 24px; border-radius: 8px; }}
     h1 {{ color: #1F4E79; }}
     </style></head><body>
-    <h1>Ung dung Flask tren PaaS</h1>
+    <h1>Ung dung Flask tren PaaS - phien ban 2!</h1>
     <div class="box">
     <p><b>Sinh vien:</b> HATRANMYKIM - 233404050183</p>
     <p><b>Mon hoc:</b> Dien toan Dam may </p>
